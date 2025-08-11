fc34f72dc9851b8efcbab03acb3ae31e563db72b
piaoxue-181 <924592920@qq.com>
2025-08-11 22:04:36 +0800

    修复

--- index.js
+++ index.js
@@ -1,42 +1,11 @@
 require("dotenv").config();
 const Koa = require("koa");
-const bodyParser = require("koa-bodyparser");
-const cors = require("koa2-cors");
 const Router = require("koa-router");
-const routes = require("./routes");
+const routes = require("./api");
 const serverless = require("serverless-http");
 
 const app = new Koa();
 const router = new Router();
-
-// 配置信息
-let domain = process.env.ALLOWED_DOMAIN || "*";
-
-// 解析请求体
-app.use(bodyParser());
-
-// 跨域
-app.use(
-  cors({
-    origin: domain,
-  })
-);
-
-app.use(async (ctx, next) => {
-  if (domain === "*") {
-    await next();
-  } else {
-    if (ctx.headers.origin === domain || ctx.headers.referer === domain) {
-      await next();
-    } else {
-      ctx.status = 403;
-      ctx.body = {
-        code: 403,
-        message: "请通过正确的域名访问",
-      };
-    }
-  }
-});
 
 // 使用路由中间件
 routes(router);
