861e0b65f079cee90eba2e5e05fb3cf19bc3aa83
piaoxue-181 <924592920@qq.com>
2025-08-11 21:25:20 +0800

    修改

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,12 @@
-const Router = require("koa-router");
-
-const router = new Router();
-
-// 根目录
-router.get("/", async (ctx) => {
-  await ctx.render("index");
-});
-
 const bing = require('./bing');
 const rss = require('./rss');
 const status = require('./status');
 
 module.exports = (router) => {
+  router.get("/", async (ctx) => {
+    await ctx.render("index");
+  });
   bing(router);
   rss(router);
   status(router);
 };
-
-// 404 路由
-router.use(async (ctx) => {
-  await ctx.render("404");
-});
-
-module.exports = router;
