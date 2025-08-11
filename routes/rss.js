cf26cde8d60ebe37bad77e2342f3ae8203237929
piaoxue-181 <924592920@qq.com>
2025-08-11 16:42:10 +0800

    一个RSS爬取工具

---
+++ routes/rss.js
@@ -0,0 +1,56 @@
+/**
+ * 爬取RSS
+ */
+
+const Router = require("koa-router");
+const rssRouter = new Router();
+const Parser = require('rss-parser');
+const parser = new Parser();
+const { get, set } = require("../utils/cacheData");
+
+// 调用路径
+const url_list = JSON.parse(process.env.RSS_URL || '[]');
+
+function sortByGmtDate(items, dateField = 'date', ascending = true) {
+  return [...items].sort((a, b) => {
+    const timeA = a[dateField] ? new Date(a[dateField]).getTime() : 0;
+    const timeB = b[dateField] ? new Date(b[dateField]).getTime() : 0;
+    return ascending ? timeA - timeB : timeB - timeA;
+  });
+}
+
+// GET
+rssRouter.get("/rss", async (ctx) => {
+  let rss_list = [];
+  // 缓存键名
+  const cacheKey = "rss_list_cache";
+  let data = await get(cacheKey);
+  if (data) {
+    ctx.body = data;
+    return;
+  }
+  for (const url of url_list) {
+    let feed = await parser.parseURL(url);
+    feed.items.forEach(item => {
+      let snippet = '';
+      if (item.contentSnippet) {
+        snippet = item.contentSnippet;
+      } else if (item.content) {
+        const plain = item.content.replace(/<[^>]+>/g, '');
+        snippet = plain.length > 200 ? plain.substring(0, 200) + '...' : plain;
+      }
+      rss_list.push({
+        "title": item.title || '',
+        "author": feed.title || '',
+        "date": item.pubDate || '',
+        "link": item.link || '',
+        "content": snippet
+      });
+    });
+  }
+  let rss_list_new = sortByGmtDate(rss_list, 'date', false);
+  await set(cacheKey, rss_list_new);
+  ctx.body = rss_list_new;
+});
+
+module.exports = rssRouter;
